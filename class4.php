559b9cd69e04658abf6d5ebab712c2b8168548eb
SohailAli <sohail.qaazi@gmail.com>
2024-02-09 20:29:24 +0500

    "Commit"

---
+++ class4.php
@@ -0,0 +1,53 @@
+<?php
+
+
+// While ///
+
+
+// $n = 1;
+
+// while($n <= 10)
+// {
+//     echo "The Number is : $n <br/>";
+// $n++;
+// }
+
+
+// Do While
+
+
+// $n = 25;
+// do{
+//     echo "The number is : $n <br/>";
+//     $n++;
+// }while($n<=20);
+
+
+// For
+
+// for($n=1;$n<=21;$n++)
+// {
+//     echo "The number is : $n <br/>";
+// }
+
+
+// For each 
+
+$color = array("red","green","black","white");
+foreach($color as $cl)
+{
+    echo "$cl <br/>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
